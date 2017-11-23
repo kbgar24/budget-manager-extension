8a106f722f10de698173c0fdf298da3233c5ed2b
Kendrick Gardner <kbgar24@gmail.com>
2017-11-23 02:37:35 -0600

    First commit

---
+++ popup.js
@@ -0,0 +1,26 @@
+$(function(){
+  $('#spendForm').submit(function(e){
+    e.preventDefault();
+  });
+
+  $('#amount').focus();
+
+  $('#spendAmount').click(function(){
+    chrome.storage.sync.get('total', function(budget){
+      var newTotal = 0;
+      if (budget.total){
+        newTotal += parseInt(budget.total);
+      }
+
+      let amount = $('#amount').val();
+      if (amount && !isNaN(amount)){
+        newTotal += parseInt(amount);
+      }
+
+      chrome.storage.sync.set({'total': newTotal});
+
+      $('#total').text(newTotal);
+      $('#amount').val('');
+    })
+  })
+})
